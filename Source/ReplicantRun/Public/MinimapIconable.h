bce6595bbfc216c5f32604266f89d4ffddafc4fd
Robin <robinandloki@gmail.com>
2025-09-22 14:28:46 +1000

    Minimap Bot Display:
    - Added MinimapIconable interface, with functions that can be implemented by a blueprint or C++ class

---
+++ Source/ReplicantRun/Public/MinimapIconable.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "MinimapIconable.generated.h"
+
+UINTERFACE(MinimalAPI, Blueprintable)
+class UMinimapIconable : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class REPLICANTRUN_API IMinimapIconable
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = MyInterface)
+	FString GetMapDisplayInfo();
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = MyInterface)
+	FVector GetObjectPostion();
+};
