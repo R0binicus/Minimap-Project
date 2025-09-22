6423f8859eb0765ecd1bc550c6ce631175670e24
Robin <robinandloki@gmail.com>
2025-09-22 15:40:41 +1000

    Minimap Bot Display: Added event dispatcher
    - Added an event dispatcher manager to be used as a temporary replacement to a subsystem

---
+++ Source/ReplicantRun/Private/EventDispatcher.cpp
@@ -0,0 +1,68 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EventDispatcher.h"
+
+#pragma region Event Manager
+// Core event manager code
+UEventDispatcher* UEventDispatcher::EventManagerSingleton = nullptr;
+
+UEventDispatcher::UEventDispatcher()
+{
+    EventManagerSingleton = this;
+}
+
+UEventDispatcher::~UEventDispatcher()
+{
+    EventManagerSingleton = nullptr;
+}
+
+UEventDispatcher* UEventDispatcher::GetEventManagerSingleton()
+{
+    return UEventDispatcher::EventManagerSingleton;
+}
+#pragma endregion Event Manager
+
+#pragma region Example
+// These are examples of how another script would use the event dispatcher
+// Essentially imagine that they are in a script other than the EventManager
+void UEventDispatcher::ExampleNoParamsHandler()
+{
+    UE_LOG(LogTemp, Log, TEXT("ExampleNoParamsHandler"));
+}
+
+void UEventDispatcher::ExampleOneParamsHandler(bool MyBool)
+{
+    UE_LOG(LogTemp, Log, TEXT("ExampleOneParamsHandler - %s"), *FString(MyBool ? "True" : "False"));
+}
+
+void UEventDispatcher::ExampleTwoParamsHandler(bool MyBool, float MyFloat)
+{
+    UE_LOG(LogTemp, Log, TEXT("ExampleTwoParamsHandler - %s %f"), *FString(MyBool ? "True" : "False"), MyFloat);
+}
+
+void UEventDispatcher::ExampleBindDispatchersCpp()
+{
+    UEventDispatcher* Singleton = GetEventManagerSingleton();
+    Singleton->Event_ExampleNoParams.AddUniqueDynamic(Singleton, &UEventDispatcher::ExampleNoParamsHandler);
+    Singleton->Event_ExampleOneParams.AddUniqueDynamic(Singleton, &UEventDispatcher::ExampleOneParamsHandler);
+    Singleton->Event_ExampleTwoParams.AddUniqueDynamic(Singleton, &UEventDispatcher::ExampleTwoParamsHandler);
+}
+
+void UEventDispatcher::ExampleUnbindDispatchersCpp()
+{
+    UEventDispatcher* Singleton = GetEventManagerSingleton();
+    Singleton->Event_ExampleNoParams.RemoveDynamic(Singleton, &UEventDispatcher::ExampleNoParamsHandler);
+    Singleton->Event_ExampleOneParams.RemoveDynamic(Singleton, &UEventDispatcher::ExampleOneParamsHandler);
+    Singleton->Event_ExampleTwoParams.RemoveDynamic(Singleton, &UEventDispatcher::ExampleTwoParamsHandler);
+}
+
+void UEventDispatcher::ExampleCallDispatchersCpp()
+{
+    UEventDispatcher* Singleton = GetEventManagerSingleton();
+
+    // Trigger event dispatchers
+    Singleton->Event_ExampleNoParams.Broadcast();
+    Singleton->Event_ExampleOneParams.Broadcast(FMath::RandBool());
+    Singleton->Event_ExampleTwoParams.Broadcast(FMath::RandBool(), FMath::RandRange(0.0f,100.0f));
+}
+#pragma endregion Example
---
+++ Source/ReplicantRun/Public/EventDispatcher.h
@@ -0,0 +1,70 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "MinimapIconable.h"
+#include "EventDispatcher.generated.h"
+
+// Delegates that can bind to multiple UFUNCTIONs simultaniously
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FEvent_ExampleNoParams);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FEvent_ExampleOneParams, bool, MyBool);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FEvent_ExampleTwoParams, bool, MyBool, float, MyFloat);
+
+UCLASS()
+class UEventDispatcher : public UBlueprintFunctionLibrary
+{
+	#pragma region Event Manager
+	// Core event manager code
+	GENERATED_BODY()
+
+private:
+	// For delegates to be blueprint assignable, they can not be static
+	// So a singleton is used
+	static UEventDispatcher* EventManagerSingleton;
+
+	// Set singleton in constructor and destructor
+	UEventDispatcher();
+	~UEventDispatcher();
+
+public: // These are the event multi dispatchers
+	UPROPERTY(BlueprintAssignable, BlueprintCallable, Category = "Create Event Dispatcher")
+	FEvent_ExampleNoParams Event_ExampleNoParams;
+
+	UPROPERTY(BlueprintAssignable, BlueprintCallable, Category = "Create Event Dispatcher")
+	FEvent_ExampleOneParams Event_ExampleOneParams;
+
+	UPROPERTY(BlueprintAssignable, BlueprintCallable, Category = "Create Event Dispatcher")
+	FEvent_ExampleTwoParams Event_ExampleTwoParams;
+
+public: 
+	UFUNCTION(BlueprintPure, Category = "Create Event Dispatcher")
+	static UEventDispatcher* GetEventManagerSingleton();
+#pragma endregion Event Manager
+
+#pragma region Example
+// These are examples of how another script would use the event dispatcher
+// Essentially imagine that they are in a script other than the EventManager
+private: // Example functions that will be called when the event is triggered
+	UFUNCTION()
+		void ExampleNoParamsHandler();
+	UFUNCTION()
+		void ExampleOneParamsHandler(bool MyBool);
+	UFUNCTION()
+		void ExampleTwoParamsHandler(bool MyBool, float MyFloat);
+
+public:
+	// Bind all example event dispatchers
+	UFUNCTION(BlueprintCallable, Category = "Create Event Dispatcher")
+		static void ExampleBindDispatchersCpp();
+	
+	// Unbind all example event dispatchers
+	UFUNCTION(BlueprintCallable, Category = "Create Event Dispatcher")
+		static void ExampleUnbindDispatchersCpp();
+
+	// Call all example event dispatchers
+	UFUNCTION(BlueprintCallable, Category = "Create Event Dispatcher")
+		static void ExampleCallDispatchersCpp();
+#pragma endregion Example
+};
