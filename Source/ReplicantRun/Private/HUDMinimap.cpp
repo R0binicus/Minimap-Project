8eb207840f349114ab86c22b5deb108db4fe7652
Robin <robinandloki@gmail.com>
2025-09-27 18:43:26 +1000

    Minimap C++ Icons: Transferred value updates to C++
    - Transferred the setting of CameraYaw, MainPlayerLocation to C++
    - Made CameraManager and PlayerSubsystem get set on construct, rather than every tick

--- Source/ReplicantRun/Private/HUDMinimap.cpp
+++ Source/ReplicantRun/Private/HUDMinimap.cpp
@@ -1,2 +1,47 @@
 #include "HUDMinimap.h"
+#include "Kismet/GameplayStatics.h" 
+#include "PlayerSubsystem.h"
 
+void UHUDMinimap::NativeOnInitialized()
+{
+
+}
+
+void UHUDMinimap::NativePreConstruct()
+{
+	const TObjectPtr<UWorld> CurrentWorld = GetWorld();
+	if (CurrentWorld)
+	{
+		PlayerSubsystem = CurrentWorld->GetSubsystem<UPlayerSubsystem>();
+
+		CameraManager = UGameplayStatics::GetPlayerCameraManager(CurrentWorld, 0);
+	}
+}
+
+void UHUDMinimap::NativeTick_Implementation(const FGeometry& MyGeometry, float InDeltaTime)
+{
+	Super::NativeTick(MyGeometry, InDeltaTime);
+
+	SetCameraYaw();
+	SetPlayerLocation();
+}
+
+void UHUDMinimap::SetCameraYaw()
+{
+	if (!CameraManager)
+	{
+		return;
+	}
+
+	CameraYaw = CameraManager->GetCameraRotation().Yaw;
+}
+
+void UHUDMinimap::SetPlayerLocation()
+{
+	if (!PlayerSubsystem)
+	{
+		return;
+	}
+
+	MainPlayerLocation = PlayerSubsystem->GetMainPlayerLocation();
+}
--- Source/ReplicantRun/Public/HUDMinimap.h
+++ Source/ReplicantRun/Public/HUDMinimap.h
@@ -4,6 +4,8 @@
 #include "Blueprint/UserWidget.h"
 #include "HUDMinimap.generated.h"
 
+class UPlayerSubsystem;
+
 /**
  * 
  */
@@ -11,6 +13,28 @@ UCLASS()
 class REPLICANTRUN_API UHUDMinimap : public UUserWidget
 {
 	GENERATED_BODY()
+
+protected:
+	void NativeOnInitialized() override;
+
+	void NativePreConstruct() override;
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Minimap Icons")
+	void NativeTick(const FGeometry& MyGeometry, float InDeltaTime);
+	void NativeTick_Implementation(const FGeometry& MyGeometry, float InDeltaTime);
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Minimap Icons")
+	TObjectPtr<UPlayerSubsystem> PlayerSubsystem;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Minimap Icons")
+	TObjectPtr<APlayerCameraManager> CameraManager;
+
+	UFUNCTION(BlueprintCallable, Category = "Minimap Icons")
+	void SetCameraYaw();
+
+	UFUNCTION(BlueprintCallable, Category = "Minimap Icons")
+	void SetPlayerLocation();
 
 public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Minimap Icons")
