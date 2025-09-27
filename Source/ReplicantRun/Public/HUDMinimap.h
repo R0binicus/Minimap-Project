65597031017a22612bfa8c6b4e853c53e5381855
Robin <robinandloki@gmail.com>
2025-09-27 21:27:07 +1000

    Minimap C++ Icons: Added variables to header file
    - Added variables that are planned to be used for the C++ implementation of the minimap icon implementation
    - Changed BPW_HeadupDisplayto no longer use HUDMinimap as a base class
    - Added BindWidget variables

--- Source/ReplicantRun/Public/HUDMinimap.h
+++ Source/ReplicantRun/Public/HUDMinimap.h
@@ -2,9 +2,12 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "Components/CanvasPanel.h"
+#include "Components/Image.h"
 #include "HUDMinimap.generated.h"
 
 class UPlayerSubsystem;
+class UMinimapIcon;
 
 /**
  * 
@@ -21,28 +24,49 @@ protected:
 
 	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Minimap Icons")
 	void NativeTick(const FGeometry& MyGeometry, float InDeltaTime);
+
 	void NativeTick_Implementation(const FGeometry& MyGeometry, float InDeltaTime);
 
 protected:
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Minimap Icons")
+	// Set in constructor
+	UPROPERTY(BlueprintReadOnly, Category = "Minimap Icons")
 	TObjectPtr<UPlayerSubsystem> PlayerSubsystem;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Minimap Icons")
+	UPROPERTY(BlueprintReadOnly, Category = "Minimap Icons")
 	TObjectPtr<APlayerCameraManager> CameraManager;
+
+	// Tick Update
+	UPROPERTY(BlueprintReadOnly, Category = "Minimap Icons")
+	float CameraYaw;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Minimap Icons")
+	FVector MainPlayerLocation;
+
+	// Constant
+	const int32 RightAngleDegrees = 90;
+
+	const float AnchorValue = 0.5f;
+
+	// Modifiable in editor
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Minimap Icons")
+	float MinimapIconCutoffWidth = 115.f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Minimap Icons")
+	float IconLocationMultiplier = 0.1f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Minimap Icons")
+	TSubclassOf<UMinimapIcon> MinimapIconClass = nullptr;
+
+	// Bind Widget
+	UPROPERTY(BlueprintReadOnly, Category = "Minimap Icons", meta = (BindWidget))
+	TObjectPtr<UCanvasPanel> IconCanvasPanel;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Minimap Icons", meta = (BindWidget))
+	TObjectPtr<UImage> MinimapImage;
 
 	UFUNCTION(BlueprintCallable, Category = "Minimap Icons")
 	void SetCameraYaw();
 
 	UFUNCTION(BlueprintCallable, Category = "Minimap Icons")
 	void SetPlayerLocation();
-
-public:
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Minimap Icons")
-	float CameraYaw;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Minimap Icons")
-	float MinimapIconCutoffWidth = 115.f;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Minimap Icons")
-	FVector MainPlayerLocation;
 };
